14237837da4cbdd8f4eb71d188d1bb6c50ea476f
Максим Егоров <eg0r0v.ma@yandex.ru>
2023-12-01 18:42:38 +0500

    7.15. Попытка 3

--- js/render.js
+++ js/render.js
@@ -6,15 +6,15 @@ const createMiniature = (picture) => {
   miniature.querySelector('.picture__img').scr = picture.url;
   miniature.querySelector('.picture__img').alt = picture.description;
   miniature.querySelector('.picture__likes').textContent = picture.likes;
-  miniature.querySelector('.picture__comments').textContent = picture.comments;
+  miniature.querySelector('.picture__comments').textContent = picture.comments.length;
   return miniature;
 };
 
 const renderMiniatures = (pictures) => {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < pictures.length; i++) {
-    fragment.append(createMiniature(pictures[i]));
-  }
+  pictures.forEach((picture) => {
+    fragment.append(createMiniature(picture));
+  });
   container.append(fragment);
 };
 
